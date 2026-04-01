4c5fe563978e6954f7379ede8a393f92c17e3477
WebDevCF2m2026 <gitweb@cf2m.be>
2026-04-01 11:26:19 +0200

    Update front controller with require_once('../config.php')

--- public/index.php
+++ public/index.php
@@ -1,3 +1,16 @@
 <?php
-echo "Ceci est notre CF 
- path : ".__dir__;
+# public/index.php 
+/****************************
+ * Chargement des dépendances
+ * ici seulement config.php
+ * qui se trouve 1 niveau en
+ * dessous
+ ****************************/
+require_once '../config.php';
+// test de la constante racine
+echo "Racine du projet : ".ROOT_PATH."
+";
+// affichage des pages acceptées
+echo "<pre>";
+print_r(ARRAY_VALID_PAGES);
+echo "</pre>";
