df99b969598940bab1c7ea27282b561ac5e902b7
WebDevCF2m2026 <gitweb@cf2m.be>
2026-04-03 11:55:27 +0200

    Update menu with ucfirst for title

--- view/inc/menu.php
+++ view/inc/menu.php
@@ -7,11 +7,14 @@ $homeActive = (isset($active))? $active : '';
     <a href="./" class="logo">DevPortfolio</a>
     <button class="menu-toggle">☰</button>
     <ul class="nav-links">
-        <li><a href="./" <?= $homeActive?>>Accueil</a></li>
-        
-        <li><a href="./?p=about">À propos</a></li>
-        <li><a href="./?p=skills">Compétences</a></li>
-        <li><a href="./?p=projects">Projets</a></li>
-        <li><a href="./?p=contact">Contact</a></li>
+        <li><a href="./" <?= $homeActive?>>Homepage</a></li>
+        <?php
+        // tant on a des pages à afficher
+        foreach(ARRAY_VALID_PAGES as $page):
+        ?>
+        <li><a href="./?p=<?= $page ?>"><?= ucfirst($page) ?></a></li>
+        <?php
+        endforeach;
+        ?>
     </ul>
 </nav>
